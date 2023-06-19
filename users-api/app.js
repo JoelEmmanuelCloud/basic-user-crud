deebe640ddc055dc6cfe6c498fbeb5131bcbc1d0
Joel Emmanuel <ejoel0035@gmail.com>
2023-06-19 14:49:42 +0100

    Set up Mongo DB connection and protect sensitive data with .gitignore

--- users-api/app.js
+++ users-api/app.js
@@ -3,6 +3,8 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+const connectDB = require('./database/connect');
+
 const authRoute = require('./routes/auth');
 const usersRoute = require('./routes/users');
 
@@ -15,7 +17,8 @@ const port = process.env.PORT || 5000;
 
 const start = async () => {
     try {
-        app.listen(port, console.log(`Server started at ${port}`));
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, console.log(`Server started at ${port}...`));
     } catch (error) {
         console.log(error);
     }
