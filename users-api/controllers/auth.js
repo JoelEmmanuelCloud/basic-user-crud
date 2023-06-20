c6f9c7a3560031ac6be7bee212f7060fe3fca495
Joel Emmanuel <ejoel0035@gmail.com>
2023-06-20 02:23:15 +0100

    Updated Unauthenticated Error File

--- users-api/controllers/auth.js
+++ users-api/controllers/auth.js
@@ -1,5 +1,8 @@
 const User = require('../models/user');
 const { StatusCodes } = require('http-status-codes');
+const { BadRequestError } = require('../errors')
+const bcrypt = require('bcryptjs');
+
 
 const register = async (req, res) => {
     const user = await User.create({ ...req.body });
