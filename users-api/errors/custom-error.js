3da1c4dcc1e6d530170ced83b55d30525b13d23d
Joel Emmanuel <ejoel0035@gmail.com>
2023-06-21 12:41:59 +0100

    Modified errors/custom.js

--- users-api/errors/custom-error.js
+++ users-api/errors/custom-error.js
@@ -1,14 +1,13 @@
 class CustomAPIError extends Error {
     constructor(message, statusCode) {
-      super(message);
-      this.statusCode = statusCode;
-      this.error = statusCode >= 400 && statusCode < 500 ? 'Not Found Error' : 'Server Error';
+    super(message);
+    this.statusCode = statusCode;
+    this.error = statusCode >= 400 && statusCode < 500 ? 'Not Found Error' : 'Server Error';
     }
-  }
-  
-  const createCustomError = (msg, statusCode) => {
+}
+
+const createCustomError = (msg, statusCode) => {
     return new CustomAPIError(msg, statusCode);
-  };
-  
-  module.exports = { createCustomError, CustomAPIError };
-  
+};
+
+module.exports = { createCustomError, CustomAPIError };
