77d552c74f4930febd511332a64790f7049657d7
Joel Emmanuel <ejoel0035@gmail.com>
2023-06-23 07:39:56 +0100

    Updated middleware/users-api.js

--- users-api/middleware/error-handler.js
+++ users-api/middleware/error-handler.js
@@ -1,9 +1,23 @@
-const { CustomAPIError } = require('../errors/custom-error')
-const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
-  return res.status(500).json({ msg: 'Something went wrong, please try again' })
-}
+const { CustomAPIError } = require('../errors/custom-error');
 
-module.exports = errorHandlerMiddleware
+const errorHandlerMiddleware = async(err, req, res, next) => {
+
+  if (err instanceof CustomAPIError) {
+    const json = err.json;
+
+    if (json.error === 'Validation error') {
+      return res.status(422).json({
+        type: json.error,
+        errors: json.errors
+      });
+    }
+    if (json.error) {
+      return res.status(json.statusCode).json(json);
+    }
+    return res.status(json.statusCode).json(json);
+  }
+
+  return res.status(500).json({ msg: err });
+};
+
+module.exports = errorHandlerMiddleware;
