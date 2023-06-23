ffe8f2f198cb9add10d422fcefced23bd207dc41
Joel Emmanuel <ejoel0035@gmail.com>
2023-06-23 07:36:06 +0100

    Updated app.js

--- users-api/app.js
+++ users-api/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('express-async-errors');
 const app = express();
 const users = require('./routes/users');
 const connectDB = require('./database/connect');
@@ -6,31 +7,23 @@ require('dotenv').config();
 const notFound = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-
 app.use(express.json());
-
-app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
-    const { message, error, errors } = err;
-  
-    res.status(statusCode).json({ statusCode, message, error, errors });
-  });
-  
 app.use('/api/v1/users', users);
 
 app.use(notFound);
 app.use(errorHandlerMiddleware);
+
 const port = process.env.PORT || 3000;
 
 const start = async () => {
-try {
+  try {
     await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
-    console.log(`Server is listening on port ${port}...`)
+      console.log(`Server is listening on port ${port}...`)
     );
-} catch (error) {
+  } catch (error) {
     console.log(error);
-}
+  }
 };
 
 start();
