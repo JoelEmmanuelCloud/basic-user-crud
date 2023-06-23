37adba68d4dcfb4f8dbc62e2cd478c4a7bf8624b
Joel Emmanuel <ejoel0035@gmail.com>
2023-06-23 07:41:27 +0100

    Modified models/user.js file

--- users-api/models/user.js
+++ users-api/models/user.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+
+const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Please provide a name.'],
     minlength: [2, 'Name must have at least 2 characters.'],
-    maxlength: [50, 'Name cannot exceed 50 characters.']
+    maxlength: [50, 'Name cannot exceed 50 characters.'],
+    trim: true
   },
   birthday: {
     type: Date,
@@ -16,7 +18,8 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide a phone number.'],
     minlength: [10, 'Phone number must have at least 10 characters.'],
-    maxlength: [15, 'Phone number cannot exceed 15 characters.']
+    maxlength: [15, 'Phone number cannot exceed 15 characters.'],
+    trim: true
   },
   email: {
     type: String,
@@ -24,54 +27,61 @@ const userSchema = new mongoose.Schema({
     unique: true,
     minlength: [5, 'Email address must have at least 5 characters.'],
     maxlength: [255, 'Email address cannot exceed 255 characters.'],
-    match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/ , 'Please provide a valid email address.']
+    match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 'Please provide a valid email address.'],
+    trim: true
   },
   password: {
     type: String,
     required: [true, 'Please provide a password.'],
     minlength: [6, 'Password must have at least 6 characters.'],
-    maxlength: [100, 'Password cannot exceed 100 characters.']
   },
-  confirmPassword: {
-    type: String,
-    required: [true, 'Please confirm the password.']
-  },
+  
   zipCode: {
     type: String,
     required: [true, 'Please provide a zip code.'],
     minlength: [5, 'Zip code must have at least 5 characters.'],
-    maxlength: [10, 'Zip code cannot exceed 10 characters.']
+    maxlength: [10, 'Zip code cannot exceed 10 characters.'],
+    trim: true
   },
   street: {
     type: String,
     required: [true, 'Please provide a street name.'],
     minlength: [2, 'Street name must have at least 2 characters.'],
-    maxlength: [100, 'Street name cannot exceed 100 characters.']
+    maxlength: [100, 'Street name cannot exceed 100 characters.'],
+    trim: true
   },
   number: {
     type: String,
     required: [true, 'Please provide a house number.'],
     minlength: [1, 'House number must have at least 1 character.'],
-    maxlength: [10, 'House number cannot exceed 10 characters.']
+    maxlength: [10, 'House number cannot exceed 10 characters.'],
+    trim: true
   },
   neighbourhood: {
     type: String,
     required: [true, 'Please provide a neighborhood.'],
     minlength: [2, 'Neighborhood must have at least 2 characters.'],
-    maxlength: [100, 'Neighborhood cannot exceed 100 characters.']
+    maxlength: [100, 'Neighborhood cannot exceed 100 characters.'],
+    trim: true
   },
   city: {
     type: String,
     required: [true, 'Please provide a city.'],
     minlength: [2, 'City must have at least 2 characters.'],
-    maxlength: [100, 'City cannot exceed 100 characters.']
+    maxlength: [100, 'City cannot exceed 100 characters.'],
+    trim: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
   }
 });
 
-// userSchema.pre('save', async function() {
-//   const salt = await bcrypt.genSalt(10);
-//   this.password = await bcrypt.hash(this.password, salt);
-// });
+
+userSchema.pre('save', async function(){
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+})
 
 const User = mongoose.model('User', userSchema);
 
